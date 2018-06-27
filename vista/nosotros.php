2d97623b3e40c276425ee65501d418a639fe3051
Unknown <max.j.s.kaechele@gmail.com>
2018-06-26 22:11:48 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ vista/nosotros.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="assets/css/styles.css">
+        <link rel="stylesheet" href="assets/css/master.css">
+        <title>Nosotros</title>
+    </head>
+    <body>
+        <header>
+          <div>
+            <ul id="menu-main">
+              <a href="#"><li>INICIO</li></a>
+              <a href="#"><li>NOSOTROS</li></a>
+              <a href="#"><li>SERVICIOS Y CATALOGO</li></a>
+              <a href="#"><li>CONTACTO</li></a>
+            </ul>
+          </div>
+        </header>
+        <article class="">
+            <h2>¿quienes somos?</h2>
+            <p></p>
+        </article>
+    </body>
+</html>
