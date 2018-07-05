b35a7b46eaff0fcb2109156e54c5a7b1285d64b4
Unknown <max.j.s.kaechele@gmail.com>
2018-07-05 16:27:48 -0400

    assets//font/galada
    assets//font/galada agregada y agregada en el css styles.css

---
+++ vista/usuario/form-nosotros.php
@@ -0,0 +1,11 @@
+<div class="contenedor">
+    <form class="form" action="?vst=2" method="post">
+        <h1>Modificar Nosotros</h1>
+        <div class="ingreso">
+            <label for="">titulo:</label>
+            <input type="text" name="qs" value="">
+            <label for="">detalle</label>
+            <textarea name="name" rows="8" cols="80"></textarea>
+        </div>
+    </form>
+</div>
